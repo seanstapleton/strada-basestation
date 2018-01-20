c39ef49a1f9ec80c5430e3b42314f257a4e51b95
Sean Stapleton <seanstapleton100@gmail.com>
2018-01-20 20:14:04 +0000

    limited checks to every 10 seconds

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,42 @@ var express = require('express');
 var router = express.Router();
 var async = require('async');
 var Estimote = require('bleacon').Estimote;
-
 var async = require('async');
+var querystring = require('querystring');
+var http = require("http");
+var fs = require("fs");
 
-var Estimote = require('./estimote.js');
+var beacons_in_range = {};
 
-Estimote.discover(function(estimote) {
+Estimote.discoverAll(function(estimote) {
   async.series([
     function(callback) {
-      estimote.on('disconnect', function() {
-        console.log('disconnected!');
-        process.exit(0);
+			if (beacons_in_range.hasOwnProperty(estimote.uuid) && new Date().getTime() - beacons_in_range[estimote.uuid].delta < 10000) return;
+			console.log('found: ' + estimote.toString());
+      var data = querystring.stringify({
+	      'id_': estimote.uuid,
+	      'beaconID': 'demo_desk_0'
       });
+      var post_options = {
+	      host: 'strada-traffic.herokuapp.com',
+	      path: '/checkIn',
+	      method: 'POST',
+	      headers: {
+		      'Content-Type': 'application/x-www-form-urlencoded',
+		      'Content-Length': Buffer.byteLength(data)
+	      }
+      };
 
-      estimote.on('motionStateChange', function(isMoving) {
-        console.log('\tmotion state change: isMoving = ' + isMoving);
+      var post_req = http.request(post_options, function(res) {
+				beacons_in_range[estimote.uuid] = estimote;
+				beacons_in_range[estimote.uuid].delta = new Date().getTime();
+	      res.setEncoding('utf8');
+	      res.on('data', function(chunk) {
+		      console.log('Response: ' + chunk);
+	      });
       });
-
-      console.log('found: ' + estimote.toString());
-
-      console.log('connectAndSetUp');
-      estimote.connectAndSetUp(callback);
+      post_req.write(data);
+      return post_req.end();
     }
   ]);
 });
